d6b37a0be7d8a68bdad8d2dd5ef5995d4bde40c1
Daniel Garcia <dagarcia625@gmail.com>
2020-12-06 10:32:58 -0500

    Updated with solutions for day 6.  Also fixed bug in trim code.

---
+++ 2020/day6.h
@@ -0,0 +1,17 @@
+//
+// Created by Daniel Garcia on 12/6/20.
+//
+
+#ifndef ADVENTOFCODE_DAY6_H
+#define ADVENTOFCODE_DAY6_H
+
+#include <filesystem>
+
+namespace aoc2020 {
+
+    void solve_day_6_1(const std::filesystem::path& input_dir);
+    void solve_day_6_2(const std::filesystem::path& input_dir);
+
+} /* namespace aoc2020 */
+
+#endif //ADVENTOFCODE_DAY6_H
