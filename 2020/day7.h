474a520cb6dc10add023fd99b5cce54c449b3801
Daniel Garcia <dagarcia625@gmail.com>
2020-12-07 21:40:09 -0500

    Updated with solutions for day 7.

---
+++ 2020/day7.h
@@ -0,0 +1,17 @@
+//
+// Created by Daniel Garcia on 12/7/20.
+//
+
+#ifndef ADVENTOFCODE_DAY7_H
+#define ADVENTOFCODE_DAY7_H
+
+#include <filesystem>
+
+namespace aoc2020 {
+
+    void solve_day_7_1(const std::filesystem::path& input_dir);
+    void solve_day_7_2(const std::filesystem::path& input_dir);
+
+} /* namespace aoc2020 */
+
+#endif //ADVENTOFCODE_DAY7_H
