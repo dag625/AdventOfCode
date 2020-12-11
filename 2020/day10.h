2c684f03f411a3a51f317459517cf30840212f04
Daniel Garcia <dagarcia625@gmail.com>
2020-12-11 00:15:12 -0500

    Updated with solutions for day 10 challenges.  Also added a timer per challenge.  This is probably noisy but still interesting.

---
+++ 2020/day10.h
@@ -0,0 +1,17 @@
+//
+// Created by Daniel Garcia on 12/10/20.
+//
+
+#ifndef ADVENTOFCODE_DAY10_H
+#define ADVENTOFCODE_DAY10_H
+
+#include <filesystem>
+
+namespace aoc2020 {
+
+    void solve_day_10_1(const std::filesystem::path& input_dir);
+    void solve_day_10_2(const std::filesystem::path& input_dir);
+
+} /* namespace aoc2020 */
+
+#endif //ADVENTOFCODE_DAY10_H
