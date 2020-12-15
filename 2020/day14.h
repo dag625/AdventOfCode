85110246a4f0ef6e5b520c3fbdc5b32343e97633
Daniel Garcia <dagarcia625@gmail.com>
2020-12-14 22:22:34 -0500

    Completed challenges for day 14.

---
+++ 2020/day14.h
@@ -0,0 +1,17 @@
+//
+// Created by Daniel Garcia on 12/14/20.
+//
+
+#ifndef ADVENTOFCODE_DAY14_H
+#define ADVENTOFCODE_DAY14_H
+
+#include <filesystem>
+
+namespace aoc2020 {
+
+    void solve_day_14_1(const std::filesystem::path& input_dir);
+    void solve_day_14_2(const std::filesystem::path& input_dir);
+
+} /* namespace aoc2020 */
+
+#endif //ADVENTOFCODE_DAY14_H
