bcb372ffc7cd33903dc74c5d1ba2022a02d3f451
Daniel Garcia <dagarcia625@gmail.com>
2020-12-15 23:12:37 -0500

    Added support for embedded unit tests via doctest.  Added more sophisticated argument parsing code to help work with doctest's arguments.

--- 2020/day15.cpp
+++ 2020/day15.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "day15.h"
+#include "span.h"
+
+#include <doctest/doctest.h>
 
 #include <iostream>
 #include <array>
@@ -14,11 +17,11 @@ namespace aoc2020 {
 
         constexpr auto INPUT = std::array{0,12,6,13,20,1,17};
 
-        uint64_t play_until(uint64_t turns) {
+        uint64_t play_until(aoc::span<const int> input, uint64_t turns) {
             //Assume turns > INPUT.size()
             std::unordered_map<uint64_t, uint64_t> numbers;
             uint64_t turn = 0;
-            for (auto v : INPUT) {
+            for (auto v : input) {
                 numbers[v] = turn;
                 ++turn;
             }
@@ -79,7 +82,7 @@ namespace aoc2020 {
     Given your starting numbers, what will be the 2020th number spoken?
     */
     void solve_day_15_1(const std::filesystem::path&) {
-        std::cout << '\t' << play_until(2020) << '\n';
+        std::cout << '\t' << play_until(INPUT, 2020) << '\n';
     }
 
     /*
@@ -95,7 +98,45 @@ namespace aoc2020 {
     Given your starting numbers, what will be the 30000000th number spoken?
     */
     void solve_day_15_2(const std::filesystem::path&) {
-        std::cout << '\t' << play_until(30000000) << '\n';
+        std::cout << '\t' << play_until(INPUT, 30000000) << '\n';
+    }
+
+    TEST_SUITE("day15" * doctest::description("Tests for day 15 challenges.")) {
+        TEST_CASE("main_example" * doctest::description("Day 15 example with input (0,3,6).")) {
+            constexpr auto TEST_INPUT = std::array{0,3,6};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 436);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 175594);
+        }
+        TEST_CASE("2nd_example" * doctest::description("Day 15 example with input (1,3,2).")) {
+            constexpr auto TEST_INPUT = std::array{1,3,2};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 1);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 2578);
+        }
+        TEST_CASE("3rd_example" * doctest::description("Day 15 example with input (2,1,3).")) {
+            constexpr auto TEST_INPUT = std::array{2,1,3};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 10);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 3544142);
+        }
+        TEST_CASE("4th_example" * doctest::description("Day 15 example with input (1,2,3).")) {
+            constexpr auto TEST_INPUT = std::array{1,2,3};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 27);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 261214);
+        }
+        TEST_CASE("5th_example" * doctest::description("Day 15 example with input (2,3,1).")) {
+            constexpr auto TEST_INPUT = std::array{2,3,1};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 78);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 6895259);
+        }
+        TEST_CASE("6th_example" * doctest::description("Day 15 example with input (3,2,1).")) {
+            constexpr auto TEST_INPUT = std::array{3,2,1};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 438);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 18);
+        }
+        TEST_CASE("7th_example" * doctest::description("Day 15 example with input (3,1,2).")) {
+            constexpr auto TEST_INPUT = std::array{3,1,2};
+            REQUIRE_EQ(play_until(TEST_INPUT, 2020), 1836);
+            REQUIRE_EQ(play_until(TEST_INPUT, 30000000), 362);
+        }
     }
 
 } /* namespace aoc2020 */
