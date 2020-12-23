f2cb7ab5b22d1b660dbc0f186b299808fb44ca80
Daniel Garcia <dagarcia625@gmail.com>
2020-12-22 21:40:52 -0500

    Completed day 21 challenges.

---
+++ 2020/day21.h
@@ -0,0 +1,17 @@
+//
+// Created by Daniel Garcia on 12/22/20.
+//
+
+#ifndef ADVENTOFCODE_DAY21_H
+#define ADVENTOFCODE_DAY21_H
+
+#include <filesystem>
+
+namespace aoc2020 {
+
+    void solve_day_21_1(const std::filesystem::path& input_dir);
+    void solve_day_21_2(const std::filesystem::path& input_dir);
+
+} /* namespace aoc2020 */
+
+#endif //ADVENTOFCODE_DAY21_H
