a9c7de06b2f203c629c869f1524792cf4655c770
Daniel Garcia <dagarcia625@gmail.com>
2021-12-10 23:00:16 -0500

    Some cleanup and prep for trying to optimize input file reading.

---
+++ file/file.h
@@ -0,0 +1,27 @@
+//
+// Created by Dan on 12/10/2021.
+//
+
+#include <string>
+#include <string_view>
+#include <vector>
+#include <memory>
+#include <filesystem>
+
+namespace aoc {
+
+//    class mapped_file {
+//        struct impl;
+//        std::unique_ptr<impl> m_impl;
+//
+//    public:
+//        enum class permission {
+//            read_only,
+//            read_write
+//        };
+//
+//        explicit mapped_file(const std::filesystem::path& file, permission perms = permission::read_only);
+//        ~mapped_file();
+//    };
+
+} /* namespace aoc */
---
+++ file/file_windows.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Dan on 12/10/2021.
+//
+
+#include "file.h"
+
+//#define WIN32_LEAN_AND_MEAN
+//#include <Windows.h>
+
+namespace aoc {
+
+//    struct mapped_file::impl {
+//        impl(const std::filesystem::path &file, permission perms) {}
+//    };
+//
+//    mapped_file::mapped_file(const std::filesystem::path &file, permission perms) :
+//        m_impl{std::make_unique<impl>(file, perms)} {}
+//
+//    mapped_file::~mapped_file() = default;
+
+} /* namespace aoc */
