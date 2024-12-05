264b15c8046bfc902ea75fbd5fe8a4602c43e0c1
Daniel Garcia <dagarcia625@gmail.com>
2024-12-05 18:14:01 -0500

    Completed 2016 day 11.  Still not fast, but finishes in 20 minutes.

--- 2016/day11.cpp
+++ 2016/day11.cpp
@@ -12,6 +12,8 @@
 #include <compare>
 #include <numeric>
 
+#include "ranges.h"
+
 namespace fs = std::filesystem;
 
 namespace {
@@ -21,18 +23,45 @@ namespace {
 
     constexpr int NUM_ITEMS_P1 = 5;
     constexpr int NUM_ITEMS_P2 = 7;
-    constexpr std::array<std::string_view, NUM_ITEMS_P2> ITEM_NAMES { "strontium"sv, "plutonium"sv, "thulium"sv, "ruthenium"sv, "curium"sv, "elerium"sv, "dilithium"sv };
+    constexpr std::array<std::string_view, NUM_ITEMS_P2> ITEM_NAMES { "thulium"sv, "ruthenium"sv, "curium"sv, "strontium"sv, "plutonium"sv, "elerium"sv, "dilithium"sv };
     constexpr int GENERATOR_OFFSET = 0;
     constexpr int CHIP_OFFSET = 1;
     constexpr int DESTINATION = 3;
+
+    bool equal_item(const int* lhs, const int* rhs) noexcept {
+        return *lhs == *rhs && *(lhs + 1) == *(rhs + 1);
+    }
+
+    template <int NUM>
+    bool equal_item(const std::array<int, NUM * 2>& lhs, const int litem, const std::array<int, NUM * 2>& rhs, const int ritem) noexcept {
+        return equal_item(lhs.data() + litem * 2, rhs.data() + ritem * 2);
+    }
+
+    template <int NUM>
+    bool equal_items(const std::array<int, NUM * 2>& lhs, const std::array<int, NUM * 2>& rhs, const int startIdx) {
+        auto comps = std::views::iota(startIdx, NUM) | std::ranges::to<std::vector>();
+        do {
+            const bool match = std::ranges::all_of(std::views::zip(comps, std::views::iota(startIdx)),
+                                                   [&lhs, &rhs](const auto& idx){ return equal_item<NUM>(lhs, std::get<0>(idx), rhs, std::get<1>(idx)); });
+            if (match) {
+                return true;
+            }
+        } while (std::next_permutation(comps.begin(), comps.end()));
+        return false;
+    }
 
     template <int NUM>
     struct stepless_state {
         int elevator = 0;
         std::array<int, NUM * 2> items{};
 
-        std::strong_ordering operator<=>(const stepless_state& rhs) const noexcept { return std::tie(elevator, items) <=> std::tie(rhs.elevator, rhs.items); }
-        bool operator==(const stepless_state& rhs) const noexcept { return std::tie(elevator, items) == std::tie(rhs.elevator, rhs.items); }
+        //std::strong_ordering operator<=>(const stepless_state& rhs) const noexcept { return std::tie(elevator, items) <=> std::tie(rhs.elevator, rhs.items); }
+        bool operator==(const stepless_state& rhs) const noexcept {
+            return elevator == rhs.elevator &&
+                    equal_item<NUM>(items, 0, rhs.items, 0) &&
+                    ((equal_item<NUM>(items, 1, rhs.items, 1) && equal_item<NUM>(items, 2, rhs.items, 2)) || (equal_item<NUM>(items, 1, rhs.items, 2) && equal_item<NUM>(items, 2, rhs.items, 1))) &&
+                    equal_items<NUM>(items, rhs.items, 3); //All items starting at index 3 all start on the ground floor.
+        }
     };
 
     template <int NUM>
@@ -47,21 +76,21 @@ namespace {
     template <int NUM>
     state<NUM> get_input(const fs::path &input_dir) {
         state<NUM> retval;
-        //strontium:
-        retval.items[0] = 0;
-        retval.items[1] = 0;
-        //plutonium:
-        retval.items[2] = 0;
-        retval.items[3] = 0;
         //thulium:
-        retval.items[4] = 1;
-        retval.items[5] = 2;
+        retval.items[0] = 1;
+        retval.items[1] = 2;
         //ruthenium:
-        retval.items[6] = 1;
-        retval.items[7] = 1;
+        retval.items[2] = 1;
+        retval.items[3] = 1;
         //curium:
-        retval.items[8] = 1;
-        retval.items[9] = 1;
+        retval.items[4] = 1;
+        retval.items[5] = 1;
+        //strontium:
+        retval.items[6] = 0;
+        retval.items[7] = 0;
+        //plutonium:
+        retval.items[8] = 0;
+        retval.items[9] = 0;
         //Note for NUM == NUM_PARTS_P2 for part 2, all the parts are on the ground level and should be set to 0,
         //which is the default/init case so I won't bother putting the explicit assignments here.
         return retval;
@@ -154,9 +183,9 @@ namespace {
             choices.erase(choices.begin());
             for (const auto& n : next) {
                 const auto sn = n.stepless();
-                const auto is_seen = std::lower_bound(seen.begin(), seen.end(), sn);
-                if (is_seen == seen.end() || *is_seen != sn) {
-                    seen.insert(is_seen, sn);
+                const auto is_seen = std::find(seen.begin(), seen.end(), sn);
+                if (is_seen == seen.end()) {
+                    seen.push_back(sn);
                     const auto mid = choices.size();
                     choices.push_back(n);
                     std::inplace_merge(choices.begin(), choices.begin() + mid, choices.end());
@@ -170,6 +199,7 @@ namespace {
 
     */
     std::string part_1(const std::filesystem::path &input_dir) {
+        //Took 12.105005800s to run on release... :/
         return std::to_string(do_with_size<NUM_ITEMS_P1>(input_dir));
     }
 
@@ -177,6 +207,7 @@ namespace {
 
     */
     std::string part_2(const std::filesystem::path &input_dir) {
+        //Took 18:40.155365 (almost 20 minutes) to run on release... :/
         return std::to_string(do_with_size<NUM_ITEMS_P2>(input_dir));
     }
 
