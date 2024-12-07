ac59fd5b8f84b70aaf6b48b993125016c09d920d
Daniel Garcia <dagarcia625@gmail.com>
2024-12-06 20:51:02 -0500

    Completed 2016 day 14.

--- 2016/day14.cpp
+++ 2016/day14.cpp
@@ -37,18 +37,30 @@ namespace {
         return h.finalizeHex();
     }
 
+    std::string calc_stretched_hash(openssl_hash& h, const std::string& salt, const int64_t index, const int repititions = 2016) {
+        auto retval = calc_hash(h, salt, index);
+        for (int i = 0; i < repititions; ++i) {
+            h.reinit();
+            h.add(retval);
+            retval = h.finalizeHex();
+        }
+        return retval;
+    }
+
     using set_state = std::array<std::vector<int64_t>, 16>;
 
     std::vector<int64_t> find_sets(const std::string& s, const int64_t index, set_state& state) {
         char last = '\0';
         int len = 0;
         std::vector<int64_t> keys;
+        bool have_triplet = false;
         for (const char c : s) {
             if (c == last) {
                 ++len;
                 int stateIndex = isalpha(c) ? (c - 'a' + 10) : (c - '0');
-                if (len == 3) {
+                if (len == 3 && !have_triplet) {
                     state[stateIndex].push_back(index);
+                    have_triplet = true;
                 }
                 else if (len == 5) {
                     keys.insert(keys.end(), state[stateIndex].begin(), state[stateIndex].end() - 1);
@@ -88,14 +100,26 @@ namespace {
         }
         std::sort(key_indicies.begin(), key_indicies.end());
         return std::to_string(key_indicies[MAX_NUM_KEYS - 1]);
-        //15183 too high
     }
 
     /************************* Part 2 *************************/
     std::string part_2(const std::filesystem::path &input_dir) {
         const auto input = get_input(input_dir);
-
-        return std::to_string(-1);
+        openssl_hash h {EVP_md5()};
+        std::vector<int64_t> key_indicies;
+        int64_t index = 0, last_key_index = 0;
+        set_state state{};
+        constexpr int MAX_NUM_KEYS = 64;
+        while (key_indicies.size() < MAX_NUM_KEYS) {
+            const auto candidate = calc_stretched_hash(h, input, index);
+            auto found = find_sets(candidate, index, state);
+            key_indicies.insert(key_indicies.end(), found.begin(), found.end());
+            ++index;
+            clear_state(index, state);
+        }
+        std::sort(key_indicies.begin(), key_indicies.end());
+        return std::to_string(key_indicies[MAX_NUM_KEYS - 1]);
+        //Takes about ~35s on release build on my computer.
     }
 
     aoc::registration r{2016, 14, part_1, part_2};
