1044d8526c933a46f0f4011318c0717ba5dc9147
Daniel Garcia <dagarcia625@gmail.com>
2024-12-08 19:30:49 -0500

    Completed 2017 day 9.

---
+++ 2017/day9.cpp
@@ -0,0 +1,111 @@
+//
+// Created by Dan on 12/8/2024.
+//
+
+#include "registration.h"
+
+#include <doctest/doctest.h>
+#include <fmt/core.h>
+
+#include <vector>
+
+#include "utilities.h"
+
+namespace fs = std::filesystem;
+
+namespace {
+
+    using namespace aoc;
+
+    /*
+     See https://adventofcode.com/2017/day/9
+     */
+
+    std::string get_input(const fs::path &input_dir) {
+        const auto lines = read_file_lines(input_dir / "2017" / "day_9_input.txt");
+        return lines.front();
+    }
+
+    int p1_score(const std::string& s) {
+        int current_group = 0;
+        int score = 0;
+        bool in_garbage = false;
+        bool ignore = false;
+        for (const char c : s) {
+            if (ignore) {
+                ignore = false;
+            }
+            else if (c == '!') {
+                ignore = true;
+            }
+            else if (in_garbage && c == '>') {
+                in_garbage = false;
+            }
+            else if (!in_garbage && c == '<') {
+                in_garbage = true;
+            }
+            else if (!in_garbage && c == '{') {
+                ++current_group;
+                score += current_group;
+            }
+            else if (!in_garbage && c == '}') {
+                --current_group;
+            }
+        }
+        return score;
+    }
+
+    int p2_score(const std::string& s) {
+        int current_group = 0;
+        int score = 0;
+        bool in_garbage = false;
+        bool ignore = false;
+        for (const char c : s) {
+            if (ignore) {
+                ignore = false;
+            }
+            else if (c == '!') {
+                ignore = true;
+            }
+            else if (in_garbage && c == '>') {
+                in_garbage = false;
+            }
+            else if (!in_garbage && c == '<') {
+                in_garbage = true;
+            }
+            else if (!in_garbage && c == '{') {
+                ++current_group;
+            }
+            else if (!in_garbage && c == '}') {
+                --current_group;
+            }
+            else if (in_garbage) {
+                ++score;
+            }
+        }
+        return score;
+    }
+
+    /************************* Part 1 *************************/
+    std::string part_1(const std::filesystem::path &input_dir) {
+        const auto input = get_input(input_dir);
+        const auto score = p1_score(input);
+        return std::to_string(score);
+    }
+
+    /************************* Part 2 *************************/
+    std::string part_2(const std::filesystem::path &input_dir) {
+        const auto input = get_input(input_dir);
+        const auto score = p2_score(input);
+        return std::to_string(score);
+    }
+
+    aoc::registration r{2017, 9, part_1, part_2};
+
+//    TEST_SUITE("2017_day09") {
+//        TEST_CASE("2017_day09:example") {
+//
+//        }
+//    }
+
+} /* namespace <anon> */
